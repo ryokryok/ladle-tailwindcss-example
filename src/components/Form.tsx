dcc2031a0ce4a6d6b1319ebe24af49a8bf4ccced
mr_ozin <mrozin.dev@gmail.com>
2023-07-22 06:53:23 +0900

    fix component type

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,29 +1,30 @@
-import React from 'react'
+import { ComponentProps, ComponentPropsWithRef, FC, forwardRef } from 'react'
 
-type InputProps = React.ComponentProps<'input'>
+type InputProps = ComponentPropsWithRef<'input'>
 
-export const Input = (props: InputProps) => (
+export const Input: FC<InputProps> = forwardRef((props, ref) => (
   <input
     className={`block border border-solid rounded border-slate-400 p-2 w-full`}
+    ref={ref}
     {...props}
   />
-)
+))
 
-type LabelProps = React.ComponentProps<'label'>
+type LabelProps = ComponentProps<'label'>
 
-export const Label = (props: LabelProps) => (
+export const Label: FC<LabelProps> = (props) => (
   <label className={`block font-bold pb-1 w-full`} {...props} />
 )
 
-type FormItemWrapperProps = React.ComponentProps<'div'>
+type FormItemWrapperProps = ComponentProps<'div'>
 
-export const FormItemWrapper = (props: FormItemWrapperProps) => (
+export const FormItemWrapper: FC<FormItemWrapperProps> = (props) => (
   <div className={`pb-4`} {...props} />
 )
 
-type FormWrapperProps = React.ComponentProps<'form'>
+type FormWrapperProps = ComponentProps<'form'>
 
-export const FormWrapper = (props: FormWrapperProps) => (
+export const FormWrapper: FC<FormWrapperProps> = (props) => (
   <form
     className={`border-2 border-solid rounded-xl border-slate-500 p-4 w-80`}
     {...props}
