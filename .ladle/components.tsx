35361a06e0058bdf686fbc3d40fe69b69533fa72
mr_ozin <mrozin.dev@gmail.com>
2023-07-22 09:55:00 +0900

    fix jsx transform

--- .ladle/components.tsx
+++ .ladle/components.tsx
@@ -1,5 +1,6 @@
 import type { GlobalProvider } from '@ladle/react'
 import '../src/index.css'
+import React from 'react'
 
 export const Provider: GlobalProvider = ({ children, globalState }) => (
   <>{children}</>
